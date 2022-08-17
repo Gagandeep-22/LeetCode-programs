06a9e7241fc5183c0ddebf343d060b69df14250a
Gagan Deep <61630537+Gagandeep-22@users.noreply.github.com>
2022-08-17 11:54:19 +0530

    Two Approaches - HashMap and Vector(ASCII) - C++

--- UniqueMorseCodeWords.cpp
+++ UniqueMorseCodeWords.cpp
@@ -41,3 +41,17 @@ public:
         return res;
     }
 };
+
+
+
+// Without HashMap
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string> d = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        unordered_set<string> s;
+        for (auto w : words) {
+            string code;
+            for (auto c : w) code += d[c - 'a'];
+            s.insert(code);
+        }
+        return s.size();
+    }
