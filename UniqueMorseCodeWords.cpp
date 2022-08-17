a4dedcb0477c553b3f2706df182495fe24c90b2c
Gagan Deep <61630537+Gagandeep-22@users.noreply.github.com>
2022-08-17 11:52:03 +0530

    Simple HashMaps - Unique MorseCode Count - C++

---
+++ UniqueMorseCodeWords.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        unordered_map<char, string> dp;
+        dp['a'] = ".-";
+        dp['b'] = "-...";
+        dp['c'] = "-.-.";
+        dp['d'] = "-..";
+        dp['e'] = ".";
+        dp['f'] = "..-.";
+        dp['g'] = "--.";
+        dp['h'] = "....";
+        dp['i'] = "..";
+        dp['j'] = ".---";
+        dp['k'] = "-.-";
+        dp['l'] = ".-..";
+        dp['m'] = "--";
+        dp['n'] = "-.";
+        dp['o'] = "---";
+        dp['p'] = ".--.";
+        dp['q'] = "--.-";
+        dp['r'] = ".-.";
+        dp['s'] = "...";
+        dp['t'] = "-";
+        dp['u'] = "..-";
+        dp['v'] = "...-";
+        dp['w'] = ".--";
+        dp['x'] = "-..-";
+        dp['y'] = "-.--";
+        dp['z'] = "--..";
+        unordered_map<string, int> count;
+        for(string word:words) {
+            string mCode = "";
+            for(auto ch:word) {
+                mCode+= dp[ch];
+            }
+            count[mCode]++;
+        }
+        int res = 0;
+        for(auto r:count) res++;
+        return res;
+    }
+};
