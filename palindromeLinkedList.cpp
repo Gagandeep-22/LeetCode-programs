9ced029b15596b3ca77ffc36a9d77a1b20a0044d
Gagan Deep <61630537+Gagandeep-22@users.noreply.github.com>
2022-08-23 17:36:04 +0530

    Palindrome Linked List. Simple approach in C++

---
+++ palindromeLinkedList.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> nums;
+        ListNode* cur = head;
+        
+        while(cur) {
+            nums.push_back(cur->val);
+            cur = cur->next;
+        }
+        int n = nums.size();
+        for(int i = 0; i < n/2; i++) {
+            if(nums[i] != nums[n-i-1]) return false;
+        }
+        return true;
+    }
+};
