238705549544797c92f9829b7d28b443d1c7ab57
Gagan Deep <61630537+Gagandeep-22@users.noreply.github.com>
2022-08-23 18:02:05 +0530

    Two approaches. O(n) and O(1) space complexity

--- palindromeLinkedList.cpp
+++ palindromeLinkedList.cpp
@@ -25,3 +25,21 @@ public:
         return true;
     }
 };
+
+// BETTER APPROACH WITH O(1) Space Complexity
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *slow = head, *fast = head, *prev, *temp;
+        while (fast && fast->next)
+            slow = slow->next, fast = fast->next->next;
+        prev = slow, slow = slow->next, prev->next = NULL;
+        while (slow)
+            temp = slow->next, slow->next = prev, prev = slow, slow = temp;
+        fast = head, slow = prev;
+        while (slow)
+            if (fast->val != slow->val) return false;
+            else fast = fast->next, slow = slow->next;
+        return true;
+    }
+};
