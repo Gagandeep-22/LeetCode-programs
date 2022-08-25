5fc268a47aa05e9c8fcad546fbe5197b328fd9e8
Gagan Deep <61630537+Gagandeep-22@users.noreply.github.com>
2022-08-25 18:54:46 +0530

    Ransome Note | Easy | C++ | HashMap

---
+++ RansomNote.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for(char m:magazine) mp[m]++;
+        for(char r:ransomNote) {
+            mp[r]--;
+            if(mp[r]<0) return false;
+        }
+        return true;
+    }
+};
